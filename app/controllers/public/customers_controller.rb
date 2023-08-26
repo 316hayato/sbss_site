6158c9f1cd258c42ff959b80aeaef9b7751499bc
316hayato <131685231+316hayato@users.noreply.github.com>
2023-08-26 02:07:26 +0000

    ユーザーの会員ページと登録内容の編集・退会処理機能作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,36 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:notice] = "更新に成功しました。"
+      redirect_to customers_my_page_path
+    else
+      flash[:alert] = "更新に失敗しました。"
+      render :edit
+    end
   end
 
   def confirm_withdraw
+  end
+
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+  
+private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
   end
 end

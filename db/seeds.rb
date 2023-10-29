7fcbc361083b49edc173e3eca567dda9cc48935e
316hayato <131685231+316hayato@users.noreply.github.com>
2023-10-29 10:27:40 +0000

    初期データ設定(修正)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "seedの実行を開始"
+
 # 管理者ログイン
 Admin.find_or_create_by!(email: "hyakkaryouran1997316@me.com") do |admin|
   admin.password = "#{ENV['SECRET_KEY']}"
@@ -15,22 +17,23 @@ end
 # 初期サービス(デフォルト)
 Service.find_or_create_by!(service_name: "保管サービス") do |service|
   service.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/save_service.jpg"), filename:"save_service.jpg")
-  service.service_name = "保管サービス",
-  service.service_introduction = "住居スペースが確保できない方におススメのサービスになります。",
-  service.price = 1500,
+  service.service_introduction = "住居スペースが確保できない方におススメのサービスになります。"
+  service.price = 1500
   service.is_active = false
 end
 
 Service.find_or_create_by!(service_name: "売却サービス") do |service|
   service.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sale_service.jpg"), filename:"sale_service.jpg")
-  service.service_introduction = "不要になった本の売却を承るサービスになります。",
-  service.price = 1500,
+  service.service_introduction = "不要になった本の売却を承るサービスになります。"
+  service.price = 1500
   service.is_active = false
 end
 
 Service.find_or_create_by!(service_name: "処分サービス") do |service|
   service.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/disposal_service.jpg"), filename:"disposal_service.jpg")
   service.service_introduction = "不要な本の処分を検討している方におススメのサービスになります。",
-  service.price = 1500,
+  service.price = 1500
   service.is_active = false
 end
+
+puts "seedの実行が完了しました"
